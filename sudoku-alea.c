ee08a4574bbda39a28210f82e757142719425f04
Supr3mSuhi <lferrand@guardiaschool.fr>
2023-03-03 01:12:55 +0100

    update

--- sudoku-alea.c
+++ sudoku-alea.c
@@ -3,7 +3,7 @@
 // Definition de la grille de Sudoku en une matrice de 9x9
 #define SIZE 9
 
-// Affichage de la grille de sudoku
+// Affichage de la grille du sudoku
 void print_grid(int grid[SIZE][SIZE]) {
     for (int row = 0; row < SIZE; row++) {
         for (int col = 0; col < SIZE; col++) {
@@ -13,23 +13,24 @@ void print_grid(int grid[SIZE][SIZE]) {
     }
 }
 
-// Vérifier si un nombre peut être placé dans une case donnée
+// Vérifie si un nombre peut être placé dans une case donnée
 int is_valid(int grid[SIZE][SIZE], int row, int col, int num) {
-    // Vérifier si le numéro existe déjà dans la ligne
+
+    // Vérifie si le numéro existe déjà dans la ligne
     for (int i = 0; i < SIZE; i++) {
         if (grid[row][i] == num) {
             return 0;
         }
     }
 
-    // Vérifier si le numéro existe déjà dans la colonne
+    // Vérifie si le numéro existe déjà dans la colonne
     for (int i = 0; i < SIZE; i++) {
         if (grid[i][col] == num) {
             return 0;
         }
     }
 
-    // Vérifier si le numéro existe déjà dans la sous-grille 3x3
+    // Vérifie si le numéro existe déjà dans la grille 3x3
     int start_row = (row / 3) * 3;
     int start_col = (col / 3) * 3;
     for (int i = start_row; i < start_row + 3; i++) {
@@ -46,22 +47,23 @@ int is_valid(int grid[SIZE][SIZE], int row, int col, int num) {
 
 // Fonction récursive pour résoudre la grille de Sudoku
 int solve_sudoku(int grid[SIZE][SIZE], int row, int col) {
-    // Si nous sommes arrivés à la fin de la grille, le Sudoku est résolu
+
+    // Si la grille est totalement remplie le sudoku est résolu
     if (row == SIZE) {
         return 1;
     }
 
-    // Si nous sommes arrivés à la fin d'une ligne, passer à la ligne suivante
+    // Si une ligne es totalement remplie, on passe à la ligne suivante
     if (col == SIZE) {
         return solve_sudoku(grid, row + 1, 0);
     }
 
-    // Si la case actuelle contient déjà un nombre, passer à la case suivante
+    // Si une case contient déjà un nombre, on passe à la case suivante
     if (grid[row][col] != 0) {
         return solve_sudoku(grid, row, col + 1);
     }
 
-    // Essayer de placer les nombres de 1 à 9 dans la case
+    // On place les nombres de 1 à 9 dans la case
     for (int num = 1; num <= 9; num++) {
         if (is_valid(grid, row, col, num)) {
             grid[row][col] = num;
@@ -69,16 +71,16 @@ int solve_sudoku(int grid[SIZE][SIZE], int row, int col) {
                 return 1;
             }
 
-            // Si le Sudoku ne peut pas être résolu avec le nombre actuel, supprimer le nombre
+            // Supprime le nombre si le sudoku n'est pas résolu avec le nombre actuel
             grid[row][col] = 0;
         }
     }
 
-    // Si aucun nombre ne fonctionne, le Sudoku est insoluble
+    // AUcun nombre fonctionne, sudoku pas resolvable
     return 0;
 }
 
-// Définir une grille de Sudoku à résoudre
+// Définition d'une grille de Sudoku à résoudre
 int main() {
     int grid[SIZE][SIZE] = {
             {5, 3, 0, 0, 7, 0, 0, 0, 0},
@@ -92,7 +94,7 @@ int main() {
             {0, 0, 0, 0, 8, 0, 0, 7, 9}
     };
 
-    // Afficher la grille de Sudoku initiale
+    // Affichage de la grille de Sudoku initiale
     printf("Grille initiale :\n");
 
     for (int row = 0; row < SIZE; row++) {
